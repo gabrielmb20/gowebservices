6ef9443ca011dcb87231a6f53d027d7a94b6da43
Gabriel <gabriel.mb20@gmail.com>
2020-08-19 20:33:12 -0600

    Tarea 2 lista

--- bookinfo_service.go
+++ bookinfo_service.go
@@ -6,6 +6,12 @@ import (
     "github.com/gofrs/uuid"
     "google.golang.org/grpc/codes"
     "google.golang.org/grpc/status"
+    "fmt"
+    "encoding/csv"
+    //"encoding/json"
+    //"io"
+    "os"
+    "log"
 )
 
 type server struct {
@@ -56,4 +62,48 @@ func (s *server) UpdateBook(ctx context.Context, in *pb.Book) (*pb.BookID, error
     }
     s.bookMap[in.Id] = in
     return &pb.BookID{Value: in.Id}, status.New(codes.OK, "").Err()
+}
+
+func (s *server) ReadCSV(ctx context.Context, in *pb.File) (*pb.BookID, error)  {
+    // Leer archivo CSV
+    f, _ := os.Open(in.Value)
+
+    // Carga todos los resgistros del archivo en un slice/array
+   bookline, err := csv.NewReader(f).ReadAll()
+   if err != nil {
+      log.Fatal(err)
+   }
+
+   if s.bookMap == nil {
+        s.bookMap = make(map[string]*pb.Book)
+    }
+
+
+   // Ciclo a traves de todos los registros, crear libro por cada uno
+   for _, book := range bookline {
+      fmt.Printf("\n\nLeyendo y creando libro con ID: ", book[0])
+      newBook, err := s.AddBook(ctx, &pb.Book{
+        Id:        book[0],
+        Title:     book[1],
+        Edition:   book[2],
+        Copyright: book[3],
+        Language:  book[4],
+        Pages:     book[5],
+        Author:    book[6],
+        Publisher: book[7]})
+
+	if err != nil {
+		log.Fatalf("\n\n\nNo se pudo agregar el libro: %v", err)
+	}
+	fmt.Printf("\n\n\nLibro agregado: ", newBook.String())
+
+	bookGet, err := s.GetBook(ctx, &pb.BookID{Value: book[0]})
+	if err != nil {
+		log.Printf("\n\n\nEl libro consultado no existe: %v", err)
+	} else {
+		log.Printf("\n\n\nConsultando libro agregado:\n\n", bookGet.String())
+	}
+   }
+
+    return &pb.BookID{Value: "1"}, status.New(codes.OK, "").Err()
 }
